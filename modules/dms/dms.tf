74435b340bcee062b0a2f508307d7ef293236026
Osi Nzeako <admin@servesolutions.info>
2024-10-07 12:47:16 -0700

    test

--- modules/dms/dms.tf
+++ modules/dms/dms.tf
@@ -37,3 +37,45 @@ resource "aws_dms_endpoint" "dms_target" {
 
   username = "test"
 }
+
+# Create a new certificate
+resource "aws_dms_certificate" "test" {
+  certificate_id  = "test-dms-certificate-tf"
+  certificate_pem = "..."
+
+  tags = {
+    Name = "test"
+  }
+
+}
+
+resource "aws_dms_event_subscription" "example" {
+  enabled          = true
+  event_categories = ["creation", "failure"]
+  name             = "my-favorite-event-subscription"
+  sns_topic_arn    = aws_sns_topic.example.arn
+  source_ids       = [aws_dms_replication_task.example.replication_task_id]
+  source_type      = "replication-task"
+
+  tags = {
+    Name = "example"
+  }
+}
+
+# Create a new replication task
+resource "aws_dms_replication_task" "test" {
+  cdc_start_time            = "1993-05-21T05:50:00Z"
+  migration_type            = "full-load"
+  replication_instance_arn  = aws_dms_replication_instance.test-dms-replication-instance-tf.replication_instance_arn
+  replication_task_id       = "test-dms-replication-task-tf"
+  replication_task_settings = "..."
+  source_endpoint_arn       = aws_dms_endpoint.test-dms-source-endpoint-tf.endpoint_arn
+  table_mappings            = "{\"rules\":[{\"rule-type\":\"selection\",\"rule-id\":\"1\",\"rule-name\":\"1\",\"object-locator\":{\"schema-name\":\"%\",\"table-name\":\"%\"},\"rule-action\":\"include\"}]}"
+
+  tags = {
+    Name = "test"
+  }
+
+  target_endpoint_arn = aws_dms_endpoint.test-dms-target-endpoint-tf.endpoint_arn
+}
+
