e14c37952e4860e33a77d3e45bd4149e414c554f
charles adejumo <admin@servesolutions.info>
2024-10-27 11:11:09 +0000

    updated nested variables

--- modules/dms/dms.tf
+++ modules/dms/dms.tf
@@ -19,15 +19,11 @@ resource "aws_dms_endpoint" "source" {
 
 # AWS DMS S3 Target Endpoint
 resource "aws_dms_s3_endpoint" "target" {
-  endpoint_id   = "donnedtipi"
-  endpoint_type = "target"
-  ssl_mode      = "none"
+  endpoint_id   = var.dms_s3_endpoint.endpoint_id
+  endpoint_type = var.dms_s3_endpoint.endpoint_type
+  ssl_mode      = var.dms_s3_endpoint.ssl_mode
 
-  tags = {
-    Name   = "donnedtipi"
-    Update = "to-update"
-    Remove = "to-remove"
-  }
+  tags = var.dms_s3_endpoint.tags
 }
 
 # AWS DMS Certificate
@@ -42,25 +38,21 @@ resource "aws_dms_certificate" "test" {
 
 # AWS DMS Replication Instance
 resource "aws_dms_replication_instance" "test" {
-  allocated_storage            = 20
-  apply_immediately            = true
-  auto_minor_version_upgrade   = true
-  availability_zone            = "us-west-2c"
-  engine_version               = "3.1.4"
-  multi_az                     = false
-  preferred_maintenance_window = "sun:10:30-sun:14:30"
-  publicly_accessible          = false
-  replication_instance_class   = "dms.t2.micro"
-  replication_instance_id      = "test-dms-replication-instance-tf"
-  allow_major_version_upgrade  = true
+  allocated_storage            = var.dms_replication_instance.allocated_storage
+  apply_immediately            = var.dms_replication_instance.apply_immediately
+  auto_minor_version_upgrade   = var.dms_replication_instance.auto_minor_version_upgrade
+  availability_zone            = var.dms_replication_instance.availability_zone
+  engine_version               = var.dms_replication_instance.engine_version
+  multi_az                     = var.dms_replication_instance.multi_az
+  preferred_maintenance_window = var.dms_replication_instance.preferred_maintenance_window
+  publicly_accessible          = var.dms_replication_instance.publicly_accessible
+  replication_instance_class   = var.dms_replication_instance.replication_instance_class
+  replication_instance_id      = var.dms_replication_instance.replication_instance_id
+  allow_major_version_upgrade  = var.dms_replication_instance.allow_major_version_upgrade
 
-  tags = {
-    Name = "test"
-  }
+  tags = var.dms_replication_instance.tags
 
-  vpc_security_group_ids = [
-    "sg-12345678",
-  ]
+  vpc_security_group_ids = var.dms_replication_instance.vpc_security_group_ids
 
   depends_on = [
     aws_iam_role_policy_attachment.dms-access-for-endpoint-AmazonDMSRedshiftS3Role,
@@ -71,31 +63,27 @@ resource "aws_dms_replication_instance" "test" {
 
 # AWS DMS Replication Task
 resource "aws_dms_replication_task" "test" {
-  cdc_start_time            = "1993-05-21T05:50:00Z"
-  migration_type            = "full-load"
-  replication_instance_arn  = aws_dms_replication_instance.test.replication_instance_arn
-  replication_task_id       = "test-dms-replication-task-tf"
-  replication_task_settings = "..."
-  source_endpoint_arn       = aws_dms_endpoint.source.endpoint_arn
-  table_mappings            = "{\"rules\":[{\"rule-type\":\"selection\",\"rule-id\":\"1\",\"rule-name\":\"1\",\"object-locator\":{\"schema-name\":\"%\",\"table-name\":\"%\"},\"rule-action\":\"include\"}]}"
+  cdc_start_time            = var.dms_replication_task.cdc_start_time
+  migration_type            = var.dms_replication_task.migration_type
+  replication_instance_arn  = var.dms_replication_task.replication_instance_arn
+  replication_task_id       = var.dms_replication_task.replication_task_id
+  replication_task_settings = var.dms_replication_task.replication_task_settings
+  source_endpoint_arn       = var.dms_replication_task.source_endpoint_arn
+  table_mappings            = var.dms_replication_task.table_mappings
 
-  tags = {
-    Name = "test"
-  }
+  tags = var.dms_replication_task.tags
 
-  target_endpoint_arn = aws_dms_s3_endpoint.target.endpoint_arn
+  target_endpoint_arn = var.dms_replication_task.target_endpoint_arn
 }
 
 # AWS DMS Event Subscription
 resource "aws_dms_event_subscription" "example" {
-  enabled          = true
-  event_categories = ["creation", "failure"]
-  name             = "my-favorite-event-subscription"
-  sns_topic_arn    = aws_sns_topic.example.arn
-  #source_ids       = [aws_dms_replication_task.test.replication_task_id]
-  source_type      = "replication-task"
+  enabled          = var.dms_event_subscription.enabled
+  event_categories = var.dms_event_subscription.event_categories
+  name             = var.dms_event_subscription.name
+  sns_topic_arn    = var.dms_event_subscription.sns_topic_arn
+  #source_ids       = var.dms_event_subscription.source_ids
+  source_type      = var.dms_event_subscription.source_type
 
-  tags = {
-    Name = "example"
-  }
+  tags = var.dms_event_subscription.tags
 }
--- modules/dms/variables.tf
+++ modules/dms/variables.tf
@@ -3,3 +3,119 @@ variable var.db_test {
   default     = ""
   description = "description"
 }
+
+variable "dms_s3_endpoint" {
+  description = "Configuration for the DMS S3 endpoint"
+  type = object({
+    endpoint_id   = string
+    endpoint_type = string
+    ssl_mode      = string
+    tags          = map(string)
+  })
+  default = {
+    endpoint_id   = "donnedtipi"
+    endpoint_type = "target"
+    ssl_mode      = "none"
+    tags = {
+      Name   = "donnedtipi"
+      Update = "to-update"
+      Remove = "to-remove"
+    }
+  }
+}
+
+variable "dms_replication_instance" {
+  description = "Configuration for the DMS replication instance"
+  type = object({
+    allocated_storage            = number
+    apply_immediately            = bool
+    auto_minor_version_upgrade   = bool
+    availability_zone            = string
+    engine_version               = string
+    multi_az                     = bool
+    preferred_maintenance_window = string
+    publicly_accessible          = bool
+    replication_instance_class   = string
+    replication_instance_id      = string
+    allow_major_version_upgrade  = bool
+    tags                         = map(string)
+    vpc_security_group_ids       = list(string)
+    depends_on                   = list(string)
+  })
+  default = {
+    allocated_storage            = 20
+    apply_immediately            = true
+    auto_minor_version_upgrade   = true
+    availability_zone            = "us-west-2c"
+    engine_version               = "3.1.4"
+    multi_az                     = false
+    preferred_maintenance_window = "sun:10:30-sun:14:30"
+    publicly_accessible          = false
+    replication_instance_class   = "dms.t2.micro"
+    replication_instance_id      = "test-dms-replication-instance-tf"
+    allow_major_version_upgrade  = true
+    tags = {
+      Name = "test"
+    }
+    vpc_security_group_ids = [
+      "sg-12345678",
+    ]
+    depends_on = [
+      "aws_iam_role_policy_attachment.dms-access-for-endpoint-AmazonDMSRedshiftS3Role",
+      "aws_iam_role_policy_attachment.dms-cloudwatch-logs-role-AmazonDMSCloudWatchLogsRole",
+      "aws_iam_role_policy_attachment.dms-vpc-role-AmazonDMSVPCManagementRole"
+    ]
+  }
+}
+
+variable "dms_replication_task" {
+  description = "Configuration for the DMS replication task"
+  type = object({
+    cdc_start_time            = string
+    migration_type            = string
+    replication_instance_arn  = string
+    replication_task_id       = string
+    replication_task_settings = string
+    source_endpoint_arn       = string
+    table_mappings            = string
+    tags                      = map(string)
+    target_endpoint_arn       = string
+  })
+  default = {
+    cdc_start_time            = "1993-05-21T05:50:00Z"
+    migration_type            = "full-load"
+    replication_instance_arn  = aws_dms_replication_instance.test.replication_instance_arn
+    replication_task_id       = "test-dms-replication-task-tf"
+    replication_task_settings = "..."
+    source_endpoint_arn       = aws_dms_endpoint.source.endpoint_arn
+    table_mappings            = "{\"rules\":[{\"rule-type\":\"selection\",\"rule-id\":\"1\",\"rule-name\":\"1\",\"object-locator\":{\"schema-name\":\"%\",\"table-name\":\"%\"},\"rule-action\":\"include\"}]}"
+    tags = {
+      Name = "test"
+    }
+    target_endpoint_arn = aws_dms_s3_endpoint.target.endpoint_arn
+  }
+}
+
+variable "dms_event_subscription" {
+  description = "Configuration for the DMS event subscription"
+  type = object({
+    enabled          = bool
+    event_categories = list(string)
+    name             = string
+    sns_topic_arn    = string
+    #source_ids       = list(string)
+    source_type      = string
+    tags = map(string)
+  })
+  default = {
+    enabled          = true
+    event_categories = ["creation", "failure"]
+    name             = "my-favorite-event-subscription"
+    sns_topic_arn    = aws_sns_topic.example.arn
+    #source_ids       = [aws_dms_replication_task.test.replication_task_id]
+    source_type      = "replication-task"
+    tags = {
+      Name = "example"
+    }
+  }
+}
